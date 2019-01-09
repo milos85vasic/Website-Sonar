9e863dc7f3eec27b74f9f794a38a8dc6d7396f3d
Milos Vasic <milos85vasic@gmail.com>
2019-01-09 10:51:26 +0100

    Website Sonar: Working on implementation.

--- sonar.py
+++ sonar.py
@@ -3,9 +3,10 @@ import time
 from configuration import *
 
 debug = True
-
+verbose = True
 working_frequency = 1
 key_frequency = 'frequency'
+default_frequency = 10 * 60 if not debug else 10
 
 elapsed_times = {}
 for website in websites:
@@ -13,15 +14,17 @@ for website in websites:
 
 
 def log(what):
-    if debug:
+    if verbose:
         print what
 
 
 def check(website, configuration):
+    log("Checking: " + website)
     return True
 
 
 def alert(website, configuration):
+    log("Website check failed: " + website)
     return
 
 
@@ -29,14 +32,10 @@ while True:
     time.sleep(working_frequency)
     for website in elapsed_times:
         elapsed_times[website] = elapsed_times[website] + 1
-        log("Website: " + website + ", elapsed: " + str(elapsed_times[website]))
-        expected_frequency = 10 * 60
+        expected_frequency = default_frequency
         if key_frequency in websites[website]:
             expected_frequency = websites[website][key_frequency]
         if elapsed_times[website] >= expected_frequency:
             elapsed_times[website] = 0
             if not check(website, websites[website]):
                 alert(website, websites[website])
-
-
-
