ae78ab4e4160c7eeba54cb03b99d5c14035e87a9
Milos Vasic <milos85vasic@gmail.com>
2019-01-20 14:58:11 +0100

    Website Sonar: Using JSON configuration.

--- README.md
+++ README.md
@@ -8,32 +8,50 @@ Tool used to periodically ping associated websites and trigger alarm if website 
 - Make sure that [Slack Python API](https://github.com/slackapi/python-slackclient) is installed
 - Clone the project
 - Initialize and clone all git submodules
-- In project root create configuration.py with the following structure (as example):
+- In project root create JSON with the following structure (as int the example):
 
-```python
-websites = {
-    "https://www.damodred.com": {},
-    "https://www.irichanin.com": {
+```json
+{
+  "websites": [
+      {
+        "url": "www.fundamental-kotlin.com",
+        "frequency": 20,
         "verification": [
-            "Agricultural holding Irichanin",
-            "Proudly powered by WordPress"
-        ],
-        "frequency": 120
-    },
-    "http://www.kolekcionari.rs": {},
-    "www.fundamental-kotlin.com": {}
+          "What is Kotlin?"
+        ]
+      },
+      {
+        "url": "https://www.damodred.com"
+      },
+      {
+        "url": "https://www.irichanin.com",
+        "frequency": 20,
+        "verification": [
+          "Agricultural holding Irichanin",
+          "Proudly powered by WordPress"
+        ]
+      },
+      {
+        "url": "http://www.kolekcionari.rs"
+      }
+    ],
+    "notification": [
+      "Slack-Notifier",
+      "Email-Notifier"
+    ],
+    "overrides": {
+      "working_frequency": 10,
+      "connectivity_verification_website": "https://www.example.com"
+    }
 }
-
-notification = ["Slack-Notifier", "Email-Notifier"]
-
-overrides = {}
 ```
 
-Where 'websites' represents dictionary with sonar urls as keys and configurations as value pairs.
+Where 'websites' represents list with website configurations.
 Each configuration contains:
     
-1. 'verification', list of strings to search for verification (if any of strings from the list is not found in url's response, sonar considers check as failure)
-2. 'frequency', check frequency in seconds.
+1. 'url', website url
+2. 'verification', list of strings to search for verification (if any of strings from the list is not found in url's response, sonar considers check as failure)
+3. 'frequency', check frequency in seconds.
 
 Websites without 'verification' values will not check against verification strings.
 Websites without 'frequency' value will be checked on every 10 minutes (default value).
@@ -42,30 +60,39 @@ Websites without 'frequency' value will be checked on every 10 minutes (default 
 
 There are two notification mechanism supported: [Slack Notifier](https://github.com/milos85vasic/Slack-Notifier) and
  [Email Notifier](https://github.com/milos85vasic/Email-Notifier).
+ 
+NOTE: Overriding 'connectivity_verification_website' parameter is mandatory!
 
 - Start website sonar:
+
+(configuration file 'configuration.json' will be used)
 ```
 $ python sonar.py
 ```
 
 or
 
+(configuration file 'my_configuration.json' will be used)
 ```
-$ python sonar.py &
+$ python sonar.py --configuration="my_configuration"
 ```
 
 or with Venv (example):
 
+(configuration file 'configuration.json' will be used)
 ```
 $ source path_to_your_venv/bin/activate; cd ~/website-sonar; python ~/website-sonar/sonar.py &
 ```
 
 Some parameters can be overridden. For example if we define overrides in our configuration.py:
 
-```python
-overrides = {
-    "working_frequency": 10
+```json
+{
+  "overrides": {
+      "working_frequency": 10,
+      "connectivity_verification_website": "https://www.example.com"
+  }
 }
 ```
 
-Website sonar's timer will tick on every 10 seconds.
+Website sonar's timer will tick on every 10 seconds and 'https://www.example.com' will be pinged for connectivity checks.
--- sonar.py
+++ sonar.py
@@ -1,13 +1,51 @@
 import os
+import sys
 import time
+import json
 import urllib2
 import requests
 import logging
+import os.path
 
 from requests import ConnectionError
 from logging.handlers import RotatingFileHandler
 
-from configuration import *
+param_configuration_name = '--configuration'
+configuration_default_file = 'configuration.json'
+
+configuration = {}
+key_websites = 'websites'
+key_overrides = 'overrides'
+key_notification = 'notification'
+key_connectivity_verification_website = 'connectivity_verification_website'
+
+
+def load_configuration():
+    configuration_file = configuration_default_file
+    for arg in sys.argv:
+        if sys.argv.index(arg) > 0:
+            if param_configuration_name in arg:
+                configuration_name = arg.replace(param_configuration_name, "")
+                configuration_name = configuration_name.replace(".json", "")
+                configuration_name = configuration_name.replace("=", "")
+                configuration_name = configuration_name.replace("'", "")
+                configuration_name = configuration_name.replace("\"", "")
+                configuration_name = configuration_name.replace(" ", "")
+                configuration_file = configuration_name + ".json"
+
+    log("Starting Website Sonar (version: " + version + "). Configuration file: " + configuration_file + ".")
+    if os.path.isfile(configuration_file):
+        try:
+            json_file = open(configuration_file)
+            json_str = json_file.read()
+            configuration.update(json.loads(json_str))
+            if key_websites in configuration and key_overrides in configuration and \
+                    key_connectivity_verification_website in configuration[key_overrides]:
+                return True
+        except:
+            return False
+    return False
+
 
 app_log = logging.getLogger('root')
 
@@ -23,7 +61,6 @@ default_frequency = 10 * 60 if not debug else 10
 key_notification_mechanism_println = "Println"
 key_notification_mechanism_slack = "Slack-Notifier"
 key_notification_mechanism_email = "Email-Notifier"
-connectivity_verification_website = "https://www.google.com"
 headers = {'user-agent': 'Website Sonar, version: ' + version}
 
 log_filename = 'website-sonar.log'
@@ -31,12 +68,10 @@ log_files_count = 10 if not debug else 5
 log_max_file_size = 5 * 1024 * 1024 if not debug else 1024
 
 elapsed_times = {}
-for item in websites:
+for item in configuration[key_websites]:
     elapsed_times[item] = 0
 
 unreachable_websites = []
-
-configuration_default_file = 'configuration.json'
 
 
 def log(what):
@@ -48,7 +83,7 @@ def log(what):
 
 def internet_on():
     try:
-        urllib2.urlopen(connectivity_verification_website, timeout=1)
+        urllib2.urlopen(configuration[key_overrides][key_connectivity_verification_website], timeout=1)
         return True
     except urllib2.URLError:
         return False
@@ -74,7 +109,7 @@ def check(website, configuration):
 
 
 def perform_check(website):
-    if check(website, websites[website]):
+    if check(website, configuration[key_websites][website]):
         message = "Website " + website + " is ok."
         if website in unreachable_websites:
             message = "Website " + website + " is reachable again."
@@ -103,16 +138,17 @@ def fail(website):
 
 
 def notify(message):
-    for mechanism in notification:
-        if mechanism == key_notification_mechanism_slack:
-            slack(message)
-            continue
-        if mechanism == key_notification_mechanism_email:
-            email(message)
-            continue
-        if mechanism == key_notification_mechanism_println:
-            print ("MSG :: " + message)
-            continue
+    if key_notification in configuration:
+        for mechanism in configuration[key_notification]:
+            if mechanism == key_notification_mechanism_slack:
+                slack(message)
+                continue
+            if mechanism == key_notification_mechanism_email:
+                email(message)
+                continue
+            if mechanism == key_notification_mechanism_println:
+                print ("MSG :: " + message)
+                continue
 
 
 def slack(message):
@@ -153,14 +189,18 @@ def run_sonar():
         app_log.setLevel(logging.DEBUG)
         app_log.addHandler(handler)
 
+    if not load_configuration():
+        log("Website Sonar (version: " + version + ") could not be started. Could not load configuration JSON.")
+        sys.exit(1)
+
     start_message = "Website Sonar (version: " + version + ") is STARTED."
-    if key_notification_mechanism_email in notification:
+    if key_notification in configuration and key_notification_mechanism_email in configuration[key_notification]:
         email(start_message)
     log(start_message)
 
     frequency = working_frequency
-    if key_working_frequency in overrides:
-        frequency = overrides[key_working_frequency]
+    if key_working_frequency in configuration[key_overrides]:
+        frequency = configuration[key_overrides][key_working_frequency]
 
     while True:
         time.sleep(frequency)
@@ -169,8 +209,8 @@ def run_sonar():
             if debug:
                 log("Tick. " + str(elapsed_times[website]))
             expected_frequency = default_frequency
-            if key_frequency in websites[website]:
-                expected_frequency = websites[website][key_frequency]
+            if key_frequency in configuration[key_websites][website]:
+                expected_frequency = configuration[key_websites][website][key_frequency]
             if elapsed_times[website] >= expected_frequency:
                 elapsed_times[website] = 0
                 if not internet_on():
