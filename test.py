957efffb294d6c94671c35a34c719dbd28df634f
Milos Vasic <milos85vasic@gmail.com>
2019-01-27 15:57:34 +0100

    Website Sonar: Implementing tests script.

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
+import sys
+
 test_prefix = "test_"
 
 echo_application_repository = "https://github.com/milos85vasic/Echo.git"
+
+
+def run_sonar_tests():
+    print "TODO."
+    sys.exit(1)
+
+
+if __name__ == '__main__':
+    run_sonar_tests()
